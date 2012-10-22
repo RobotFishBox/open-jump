a7ff4f6395acfd82c47b484b72e16111e1235f74
pichangetheworld <pichangetheworld@003b7468-2fd0-de46-e96d-d469ec9a9f9a>
2012-10-22 12:46:58 +0000

    try this

---
+++ trunk/OpenJumpApp/src/clevermushroom/openjump/MainGamePanel.java
@@ -0,0 +1,35 @@
+package clevermushroom.openjump;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+	public MainGamePanel(Context context) {
+		super(context);
+		
+		getHolder().addCallback(this);
+		setFocusable(true);
+	}
+	
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
